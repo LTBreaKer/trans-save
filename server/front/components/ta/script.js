5be41dad032c3b24600a576a1dee0d4d36f309a4
Aboubaker Fanti <afanti@c3r5p6.1337.ma>
2024-09-11 22:37:49 +0100

    fanti

--- server/front/components/ta/script.js
+++ server/front/components/ta/script.js
@@ -44,6 +44,15 @@ async function localgame_tag() {
     });
     console.log(response);
     const jsonData = await response.json();
+    if (jsonData.message === "player is already in a game") {
+      console.log('hdsklfjsldkjflsdjk    => :', jsonData.message);
+      document.querySelector('.success_update').style.display = "flex";
+      setTimeout(function() {
+        document.querySelector('.success_update').style.display = 'none';
+    }, 2000);
+  
+    }
+    console.log(jsonData.message)
     const game_id =  jsonData.game_id;
     const player1_name =  jsonData.player1_name;
     const player2_name =  jsonData.player2_name;
@@ -54,16 +63,15 @@ async function localgame_tag() {
     }
     if (response.status === 201)
       window.location.hash = '/game'
+    // console.log(jsonData.message)
     
     if (!response.ok) {
+      // console.log(jsonData.message)
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error);
   }
-  
-
 }
 
 async function changeAccess() {
