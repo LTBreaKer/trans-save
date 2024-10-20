8e3a07d289327f535c4b824b616c2be787b7aeed
Abdelouakil Moulou <moabdelo@e2r9p4.1337.ma>
2024-10-20 21:26:42 +0100

    mac problem

--- server/front/pong-game/src/game/game.js
+++ server/front/pong-game/src/game/game.js
@@ -108,7 +108,7 @@ function resizeRendererToDisplaySize(renderer) {
 	const needResize = canvas.width !== width || canvas.height !== height;
 	if (needResize) {
 		renderer.setSize(width, height, false);
-		resizeCanvas();
+		// resizeCanvas();
 	}
 	return (needResize);
 }
